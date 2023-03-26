6bee71466e0d4dee84900f3d82ec2a4f7dd570f8
문성현 <shine7329@gmail.com>
2023-03-27 08:06:23 +0900

    style: change function  to arrow function

--- service/post.ts
+++ service/post.ts
@@ -1,31 +1,31 @@
-import { isFullPage } from '@notionhq/client';
-import { notion } from './notion';
+import { Client, isFullPage } from '@notionhq/client';
 import type { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 import type { Post } from '@/types/post';
 
-function getPostOrder(page: PageObjectResponse): number {
+const getPostOrder = (page: PageObjectResponse): number => {
   return (page.properties.order.type === 'number' && page.properties.order.number) || Infinity;
-}
+};
 
-function byPublic(page: PageObjectResponse): boolean {
+const byPublic = (page: PageObjectResponse): boolean => {
   return page.properties.public.type === 'checkbox' && page.properties.public.checkbox;
-}
+};
 
-function byOrder(a: PageObjectResponse, b: PageObjectResponse): number {
+const byOrder = (a: PageObjectResponse, b: PageObjectResponse): number => {
   return getPostOrder(a) - getPostOrder(b);
-}
+};
 
-function convertPost(page: PageObjectResponse): Post {
+const convertPost = (page: PageObjectResponse): Post => {
   return {
     id: page.id,
     title: page.properties.name.type === 'title' ? page.properties.name.title[0].plain_text : '',
-    tags: page.properties.tags.type === 'multi_select' ? page.properties.tags.multi_select : [],
+    tagList: page.properties.tags.type === 'multi_select' ? page.properties.tags.multi_select : [],
   };
-}
+};
 
-export default async function getPostList(): Promise<Post[]> {
+const notion = new Client({ auth: process.env.NOTION_TOKEN });
+
+export const getPostList = async (): Promise<Post[]> => {
   if (!process.env.NOTION_DATABASE_ID) throw new Error('NOTION_DATABASE_ID is not defined.');
   const { results } = await notion.databases.query({ database_id: process.env.NOTION_DATABASE_ID });
-
   return results.filter(isFullPage).filter(byPublic).sort(byOrder).map(convertPost);
-}
+};
