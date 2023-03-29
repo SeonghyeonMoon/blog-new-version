e891a7b2145b1461c66767960f552c09e9cef1a3
문성현 <shine7329@gmail.com>
2023-03-29 21:48:26 +0900

    feat: add DarkModeToggleButton

---
+++ components/DarkModeToggleButton.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const DarkModeToggleButton = () => {
+  const [isDark, setIsDark] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+  const toggleColorMode = () => {
+    setIsDark(!isDark);
+  };
+
+  useEffect(() => {
+    isDark ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark');
+  }, [isDark]);
+
+  return (
+    <button onClick={toggleColorMode} className='relative h-7 w-12 rounded-full bg-blue-light dark:bg-hr-dark'>
+      <div
+        className='z-10 h-5 w-5 transform rounded-full bg-font-dark shadow-md transition-transform duration-200'
+        style={{ transform: isDark ? 'translateX(120%)' : 'translateX(20%)' }}
+      />
+    </button>
+  );
+};
+
+export default DarkModeToggleButton;
