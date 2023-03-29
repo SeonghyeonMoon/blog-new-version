5b86787e3bb9c155349351f685c22a9e38c965a8
문성현 <shine7329@gmail.com>
2023-03-29 20:42:46 +0900

    feat: add getBlockList

--- service/post.ts
+++ service/post.ts
@@ -1,6 +1,8 @@
-import { Client, isFullPage } from '@notionhq/client';
-import type { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+import { Client, isFullBlock, isFullPage } from '@notionhq/client';
+import bindListBlocks from '@/utils/bindListBlocks';
+import convertBlock from '@/utils/convertBlock';
 import type { PostType } from '@/types/post';
+import type { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 
 const getPostOrder = (page: PageObjectResponse): number => {
   return (page.properties.order.type === 'number' && page.properties.order.number) || Infinity;
@@ -34,4 +36,27 @@ export const getPost = async (id: string): Promise<PostType> => {
   const page = await notion.pages.retrieve({ page_id: id });
   if (!isFullPage(page)) throw new Error('Page is not found.');
   return convertPost(page);
+};
+
+export const getBlockList = async (blockId: string) => {
+  const { results } = await notion.blocks.children.list({ block_id: blockId });
+  const blocks = await Promise.all(results.filter(isFullBlock).map(convertBlock));
+
+  const hasChildrenBlocks = blocks.filter((block) => block?.hasChildren);
+  while (hasChildrenBlocks.length > 0) {
+    const block = hasChildrenBlocks.pop();
+    if (!block) continue;
+    const { results } = await notion.blocks.children.list({ block_id: block.id });
+    const blockObjectResponse = await Promise.all(results.filter(isFullBlock).map(convertBlock));
+
+    // @ts-ignore
+    block.children = blockObjectResponse.map((block) => {
+      if (block?.hasChildren) {
+        hasChildrenBlocks.push(block);
+      }
+      return block;
+    });
+  }
+
+  return bindListBlocks(blocks);
 };
