ddbe8001f72a770b0f28601aa7e77bf3ae27ffc4
문성현 <shine7329@gmail.com>
2023-03-31 16:48:08 +0900

    feat: add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='mx-auto max-w-3xl'>
+      <p className='py-10 text-center'>© Copyright 2023. Moon All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
