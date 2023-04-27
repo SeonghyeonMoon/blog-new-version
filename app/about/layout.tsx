586db4095f45e7c210c449702fd0e9c8d158512d
문성현 <shine7329@gmail.com>
2023-04-27 21:52:19 +0900

    feat: add metadata to AboutPage

---
+++ app/about/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'About',
+};
+
+const AboutLayout = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
+
+export default AboutLayout;
